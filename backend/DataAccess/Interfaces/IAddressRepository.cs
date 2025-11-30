1570e07125b0c340c29b8085abcf4d6e437dc508
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-11-30 21:13:36 +0200

    Add Address Repository

---
+++ backend/DataAccess/Interfaces/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.DataAccess.Interfaces
+{
+    public interface IAddressRepository
+    {
+        public Task<Result<AddressDTO>> AddNewAsync(AddressDTO newAddress);
+        public Task<Result<bool>> UpdateAsync(int id, AddressDTO updatedAddress);
+        public Task<Result<bool>> DeleteAsync(int id);
+        public Task<Result<AddressDTO>> GetById(int id);
+        public Task<Result<IEnumerable<AddressDTO>>> GetAllAsync(int person_id);
+    }
+}
