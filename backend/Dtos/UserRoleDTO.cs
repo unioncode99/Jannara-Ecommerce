2061b98858779d76365b1b886b921621f0a12d7a
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-11-30 15:15:51 +0200

    Create Address, Role, UserRole DTOs

---
+++ backend/Dtos/UserRoleDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Jannara_Ecommerce.Dtos
+{
+    public class UserRoleDTO
+    {
+        public UserRoleDTO(int id, int roleId, int userId, bool isActive, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            RoleId = roleId;
+            UserId = userId;
+            this.isActive = isActive;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+        public bool isActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
