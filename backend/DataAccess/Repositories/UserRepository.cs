2b932789e1e9e58057ca9000ced0ec4f1e980b50
muzamil <muzamilalfatih123@gmail.com>
2025-11-30 20:30:16 +0200

    create seller repository

--- backend/DataAccess/Repositories/UserRepository.cs
+++ backend/DataAccess/Repositories/UserRepository.cs
@@ -61,7 +61,7 @@ Select * from Users Where Id  = (SELECT SCOPE_IDENTITY());
                                );
                                 return new Result<UserPublicDTO>(true, "User added successfully.", insertedUser);
                             }
-                            return new Result<UserPublicDTO>(false, "Failed to add User.", null);
+                            return new Result<UserPublicDTO>(false, "Failed to add User.", null, 500);
 
                         }
 
@@ -91,13 +91,8 @@ Select * from Users Where Id  = (SELECT SCOPE_IDENTITY());
                         object? result = await command.ExecuteScalarAsync();
                         int rowAffected = result != DBNull.Value ? Convert.ToInt32(result) : 0;
                         if (rowAffected > 0)
-                        {
                             return new Result<bool>(true, "User deleted successfully.", true);
-                        }
-                        else
-                        {
-                            return new Result<bool>(false, "Failed to delete user.", false);
-                        }
+                        return new Result<bool>(false, "User not found.", false, 404);
                     }
                     catch (Exception ex)
                     {
@@ -142,9 +137,9 @@ OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY ;";
                             }
 
                             if (users.Count() > 0)
-                                return new Result<IEnumerable<UserPublicDTO>>(true, "Doctors retrieved successfully", users);
+                                return new Result<IEnumerable<UserPublicDTO>>(true, "Users retrieved successfully", users);
                             else
-                                return new Result<IEnumerable<UserPublicDTO>>(false, "No doctor found!", null, 404);
+                                return new Result<IEnumerable<UserPublicDTO>>(false, "No user found!", null, 404);
 
                         }
                     }
@@ -188,7 +183,7 @@ Select * from Users where email = @email;
                                );
                                 return new Result<UserDTO>(true, "User retrieved successfully.", user);
                             }
-                            return new Result<UserDTO>(false, "Failed to retrieved User.", null);
+                            return new Result<UserDTO>(false, "User not found.", null, 404);
 
                         }
 
@@ -234,7 +229,7 @@ Select * from Users where id = @id;
                                );
                                 return new Result<UserDTO>(true, "User retrieved successfully.", user);
                             }
-                            return new Result<UserDTO>(false, "Failed to retrieved User.", null);
+                            return new Result<UserDTO>(false, "User not found.", null, 404);
 
                         }
 
@@ -277,13 +272,8 @@ select @@ROWCOUNT";
                         object? result = await command.ExecuteScalarAsync();
                         int rowAffected = result != DBNull.Value ? Convert.ToInt32(result) : 0;
                         if (rowAffected > 0)
-                        {
                             return new Result<bool>(true, "user updated successfully.", true);
-                        }
-                        else
-                        {
-                            return new Result<bool>(false, "Failed to update user.", false);
-                        }
+                        return new Result<bool>(false, "Failed to update user.", false);
                     }
                     catch (Exception ex)
                     {
