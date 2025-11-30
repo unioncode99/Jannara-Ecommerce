bca24eec8c5a7ff83b78c78274f8d0cabe88aed8
muzamil <muzamilalfatih123@gmail.com>
2025-11-30 15:09:40 +0200

    Create cutomers, seller, user dtos

---
+++ backend/Dtos/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Jannara_Ecommerce.Dtos
+{
+    public class UserDTO
+    {
+        public UserDTO(int id, int personId, string email, string username,
+            string password, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            PersonId = personId;
+            Email = email;
+            Username = username;
+            Password = password;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
