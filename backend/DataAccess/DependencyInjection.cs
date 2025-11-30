3cefdbdf1bb88d7a69937e6a3d379e1d2851f93d
muzamil <muzamilalfatih123@gmail.com>
2025-11-30 20:34:36 +0200

    create dataaccess dependeny injection

---
+++ backend/DataAccess/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace Jannara_Ecommerce.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationRepository (this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
