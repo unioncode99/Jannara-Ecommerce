a730266fc60918a6eff73011ba5efb5f63965209
muzamil <muzamilalfatih123@gmail.com>
2025-11-30 14:55:28 +0200

    add dto and enum folders

---
+++ backend/Dtos/PersonDTO.cs
@@ -0,0 +1,31 @@
+﻿using Jannara_Ecommerce.Enums;
+
+namespace Jannara_Ecommerce.Dtos
+{
+    public class PersonDTO
+    {
+        public PersonDTO(int id, string firstName, string lastName, string phone,
+            string imageUrl, Gender gender, DateOnly dateOfBirth, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            ImageUrl = imageUrl;
+            Gender = gender;
+            DateOfBirth = dateOfBirth;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+        public Gender Gender { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
