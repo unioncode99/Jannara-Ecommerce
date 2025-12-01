b747fda129f940d6334dce66147325051292f101
muzamil <muzamilalfatih123@gmail.com>
2025-12-01 09:53:01 +0200

    addd customer and seller services

---
+++ backend/Business/Interfaces/ISellerService.cs
@@ -0,0 +1,13 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Interfaces
+{
+    public interface ISellerService
+    {
+        public Task<Result<SellerDTO>> FindAsync(int id);
+        public Task<Result<SellerDTO>> AddNewAsync(SellerDTO newSeller);
+        public Task<Result<bool>> UpdateAsync(int id, SellerDTO updatedSeller);
+        public Task<Result<bool>> DeleteAsync(int id);
+    }
+}
---
+++ backend/Business/Services/CustomerService.cs
@@ -0,0 +1,35 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.DataAccess.Interfaces;
+using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ICustomerRepository _repo;
+        public CustomerService(ICustomerRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Result<CustomerDTO>> AddNewAsync(CustomerDTO newCustomer)
+        {
+            return await _repo.AddNewAsync(newCustomer);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(int id)
+        {
+            return await _repo.DeleteAsync(id);
+        }
+
+        public async Task<Result<CustomerDTO>> FindAsync(int id)
+        {
+            return await _repo.GetByIdAsync(id);
+        }
+
+        public async Task<Result<bool>> UpdateAsync(int id, CustomerDTO updatedCustomer)
+        {
+            return await _repo.UpdateAsync(id, updatedCustomer);
+        }
+    }
+}
