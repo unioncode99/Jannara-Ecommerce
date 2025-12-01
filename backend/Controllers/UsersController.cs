ecc61c97dbb2ba1459b69fd5c381a6a4a5074764
muzamil <muzamilalfatih123@gmail.com>
2025-12-01 15:18:03 +0200

    create user controller

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,81 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.DTOs.Mappers;
+using Jannara_Ecommerce.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jannara_Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}", Name = "GetUserByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<UserDTO>> GetUserByID(int id)
+        {
+            Result<UserDTO> result = await _service.FindAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data.ToUserPublicDTO());
+            }
+            return StatusCode(result.ErrorCode, result.Message);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<UserPublicDTO>> AddUser(CreateNewUserDTO newUser)
+        {
+            Result<UserPublicDTO> result = await _service.CreateAsync(newUser); 
+            if (result.IsSuccess)
+            {
+                return CreatedAtRoute("GetUserByID", new { id = result.Data.Id }, result.Data);
+            }
+            return StatusCode(result.ErrorCode, result.Message);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> UpdateDoctor(int id, [FromBody] UserDTO updatedUser)
+        {
+            Result<bool> result = await _service.UpdateAsync(id, updatedUser);
+
+            if (!result.IsSuccess)
+                return StatusCode(result.ErrorCode, result.Message);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteCustomer(int id)
+        {
+            Result<bool> result = await _service.DeleteAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+            return StatusCode(result.ErrorCode, result.Message);
+        }
+    }
+}
