64943df60475bb499856c8d2b6df26865330f00a
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-01 09:48:10 +0200

    Add Business Dependency Injection class

---
+++ backend/Business/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.Business.Services;
+
+namespace Jannara_Ecommerce.Business
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<IPersonService, PersonService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IUserRoleService, UserRoleService>();
+            return services;
+        }
+    }
+}
