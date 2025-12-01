582247b7c8566347ae8814ae94b3c764db842036
muzamil <muzamilalfatih123@gmail.com>
2025-12-01 13:34:00 +0200

    Demo commit

--- backend/Business/Interfaces/IPersonService.cs
+++ backend/Business/Interfaces/IPersonService.cs
@@ -6,6 +6,7 @@ namespace Jannara_Ecommerce.Business.Interfaces
     public interface IPersonService
     {
         public Task<Result<PersonDTO>> FindAsync(int id);
+        public Task<Result<string>> _handleSaveImage(IFormFile image);
         public Task<Result<PersonDTO>> AddNewAsync(PersonDTO newPerson);
         public Task<Result<bool>> UpdateAsync(int id, PersonDTO updatedPerson);
         public Task<Result<bool>> DeleteAsync(int id);
--- backend/Dtos/PersonDTO.cs
+++ backend/Dtos/PersonDTO.cs
@@ -4,14 +4,15 @@ namespace Jannara_Ecommerce.DTOs
 {
     public class PersonDTO
     {
-        public PersonDTO(int id, string firstName, string lastName, string phone,
-            string imageUrl, Gender gender, DateOnly dateOfBirth, DateTime createdAt, DateTime updatedAt)
+        public PersonDTO(int id, string firstName, string lastName, string phone, string? imageUrl, IFormFile? profileImage,
+            Gender gender, DateOnly dateOfBirth, DateTime createdAt, DateTime updatedAt)
         {
             Id = id;
             FirstName = firstName;
             LastName = lastName;
             Phone = phone;
             ImageUrl = imageUrl;
+            ProfileImage = profileImage;
             Gender = gender;
             DateOfBirth = dateOfBirth;
             CreatedAt = createdAt;
@@ -22,7 +23,8 @@ namespace Jannara_Ecommerce.DTOs
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
+        public IFormFile? ProfileImage { get; set; }
         public Gender Gender { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public DateTime CreatedAt { get; set; }
