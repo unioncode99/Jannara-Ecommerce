4debfb0d24d9661673753692f0d0108a61935dc4
muzamil <muzamilalfatih123@gmail.com>
2025-12-01 09:53:01 +0200

    addd customer and seller services

---
+++ backend/Business/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Interfaces
+{
+    public interface ICustomerService
+    {
+        public Task<Result<CustomerDTO>> FindAsync(int id);
+        public Task<Result<CustomerDTO>> AddNewAsync(CustomerDTO newCustomer);
+        public Task<Result<bool>> UpdateAsync(int id, CustomerDTO updatedCustomer);
+        public Task<Result<bool>> DeleteAsync(int id);
+    }
+}
