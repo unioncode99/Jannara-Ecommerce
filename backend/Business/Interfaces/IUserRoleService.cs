b067c3a92f3993a27b4c8444aff1f6f4522ac722
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-01 09:41:58 +0200

    Add UserRole Service

---
+++ backend/Business/Interfaces/IUserRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Interfaces
+{
+    public interface IUserRoleService
+    {
+        public Task<Result<UserRoleDTO>> FindAsync(int id);
+        public Task<Result<UserRoleDTO>> AddNewAsync(UserRoleDTO newUserRole);
+        public Task<Result<bool>> UpdateAsync(int id, UserRoleDTO updatedUserRole);
+        public Task<Result<bool>> DeleteAsync(int id);
+    }
+}
