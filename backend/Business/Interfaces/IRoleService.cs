9468ad42e84b8a1bd29dc59e7748fd15d102d0cd
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-01 09:36:51 +0200

    Add Role Service

---
+++ backend/Business/Interfaces/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Interfaces
+{
+    public interface IRoleService
+    {
+        public Task<Result<RoleDTO>> FindAsync(int id);
+        public Task<Result<RoleDTO>> AddNewAsync(RoleDTO newRole);
+        public Task<Result<bool>> UpdateAsync(int id, RoleDTO updatedRole);
+        public Task<Result<bool>> DeleteAsync(int id);
+    }
+}
---
+++ backend/Business/Services/RoleService.cs
@@ -0,0 +1,35 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.DataAccess.Interfaces;
+using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+        public RoleService(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+        public async Task<Result<RoleDTO>> AddNewAsync(RoleDTO newRole)
+        {
+            return await _roleRepository.AddNewAsync(newRole);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(int id)
+        {
+            return await _roleRepository.DeleteAsync(id);
+        }
+
+        public async Task<Result<RoleDTO>> FindAsync(int id)
+        {
+            return await _roleRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Result<bool>> UpdateAsync(int id, RoleDTO updatedRole)
+        {
+            return await _roleRepository.UpdateAsync(id, updatedRole);  
+        }
+    }
+}
