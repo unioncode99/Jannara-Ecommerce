abb5d696921dab31cdf62ab069060df68df34825
muzamil <muzamilalfatih123@gmail.com>
2025-12-01 17:17:02 +0200

    create people controller

--- backend/Controllers/PeopleController.cs
+++ backend/Controllers/PeopleController.cs
@@ -1,8 +1,6 @@
 ﻿using Jannara_Ecommerce.Business.Interfaces;
 using Jannara_Ecommerce.DTOs;
-using Jannara_Ecommerce.DTOs.Mappers;
 using Jannara_Ecommerce.Utilities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,53 +8,39 @@ namespace Jannara_Ecommerce.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class PeopleController : ControllerBase
     {
-        private readonly IUserService _service;
-        public UsersController(IUserService service)
+        private readonly IPersonService _service;
+        public PeopleController(IPersonService service)
         {
             _service = service;
         }
-
-        [HttpGet("{id}", Name = "GetUserByID")]
+        [HttpGet("{id}", Name = "GetPersonByID")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<UserDTO>> GetUserByID(int id)
+        public async Task<ActionResult<PersonDTO>> GetPersonByID(int id)
         {
-            Result<UserDTO> result = await _service.FindAsync(id);
+            Result<PersonDTO> result = await _service.FindAsync(id);
             if (result.IsSuccess)
             {
-                return Ok(result.Data.ToUserPublicDTO());
+                return Ok(result.Data);
             }
             return StatusCode(result.ErrorCode, result.Message);
         }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<UserPublicDTO>> AddUser(CreateNewUserDTO newUser)
-        {
-            Result<UserPublicDTO> result = await _service.CreateAsync(newUser); 
-            if (result.IsSuccess)
-            {
-                return CreatedAtRoute("GetUserByID", new { id = result.Data.Id }, result.Data);
-            }
-            return StatusCode(result.ErrorCode, result.Message);
-        }
 
-        [HttpPut("{id}")]
+        [HttpPut("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult> UpdateUser(int id, [FromBody] UserDTO updatedUser)
+        public async Task<ActionResult> UpdatePerson(int id, [FromBody] PersonDTO updatedPerson)
         {
-            Result<bool> result = await _service.UpdateAsync(id, updatedUser);
+            Result<bool> result = await _service.UpdateAsync(id, updatedPerson);
 
             if (!result.IsSuccess)
                 return StatusCode(result.ErrorCode, result.Message);
@@ -68,7 +52,7 @@ namespace Jannara_Ecommerce.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult> DeleteUser(int id)
+        public async Task<ActionResult> DeletePerson(int id)
         {
             Result<bool> result = await _service.DeleteAsync(id);
             if (result.IsSuccess)
