2430a56f8d79f087cc4cc8bf7455d8ce56eeed1f
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-01 09:29:49 +0200

    Add Address Service

---
+++ backend/Business/Interfaces/IAddressService.cs
@@ -0,0 +1,14 @@
+﻿using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Interfaces
+{
+    public interface IAddressService
+    {
+        public Task<Result<AddressDTO>> FindAsync(int id);
+        public Task<Result<AddressDTO>> AddNewAsync(AddressDTO newAddress);
+        public Task<Result<bool>> UpdateAsync(int id, AddressDTO updatedAddress);
+        public Task<Result<bool>> DeleteAsync(int id);
+
+    }
+}
---
+++ backend/Business/Services/AddressService.cs
@@ -0,0 +1,36 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.DataAccess.Interfaces;
+using Jannara_Ecommerce.DTOs;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Services
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IAddressRepository _addressRepository;
+        public AddressService(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public async Task<Result<AddressDTO>> AddNewAsync(AddressDTO newAddress)
+        {
+            return await _addressRepository.AddNewAsync(newAddress);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(int id)
+        {
+            return await _addressRepository.DeleteAsync(id);
+        }
+
+        public async Task<Result<AddressDTO>> FindAsync(int id)
+        {
+            return await _addressRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Result<bool>> UpdateAsync(int id, AddressDTO updatedAddress)
+        {
+            return await _addressRepository.UpdateAsync(id, updatedAddress);
+        }
+    }
+}
