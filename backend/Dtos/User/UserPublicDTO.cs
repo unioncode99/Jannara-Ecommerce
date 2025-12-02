76de2c3bf9c3d67c47b125e003b887b6c36f5dfe
muzamil <muzamilalfatih123@gmail.com>
2025-12-02 14:40:12 +0200

    refactor GetAllAsync method in user repository

--- backend/Dtos/User/UserPublicDTO.cs
+++ backend/Dtos/User/UserPublicDTO.cs
@@ -7,7 +7,7 @@ namespace Jannara_Ecommerce.DTOs.User
         public UserPublicDTO(int id, int personId, string email, string username,
             DateTime createdAt, DateTime updatedAt, List<UserRoleInfoDTO> roles)
         {
-            Id = id;
+            Id = id
             PersonId = personId;
             Email = email;
             Username = username;
@@ -21,6 +21,6 @@ namespace Jannara_Ecommerce.DTOs.User
         public string Username { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        public List<UserRoleInfoDTO> Roles { get; set; }  
+        public List<UserRoleInfoDTO> Roles { get; set; }   = new List<UserRoleInfoDTO>();
     }
 }
