8c04134535c07c8498d45b2f295cb29f32dff4dc
muzamil <muzamilalfatih123@gmail.com>
2025-12-05 10:24:56 +0200

    WIP: add skeleton for LoginResponseDTO for upcoming login feature

---
+++ backend/Dtos/Authentication/LoginResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using Jannara_Ecommerce.DTOs.Person;
+using Jannara_Ecommerce.DTOs.User;
+
+namespace Jannara_Ecommerce.DTOs.Authentication
+{
+    public class LoginResponseDTO<T> where T : class
+    {
+        public LoginResponseDTO(PersonDTO person, UserPublicDTO user, Dictionary<string, T> roleData)
+        {
+            Person = person;
+            User = user;
+            RoleData = roleData;
+        }
+
+        public PersonDTO Person { get; set; }
+        public UserPublicDTO User { get; set; }
+        public Dictionary<string, T> RoleData { get; set; }
+    }
+}
