f6ffda3403e190f8e1f8295ae54bfca722156f66
muzamil <muzamilalfatih123@gmail.com>
2025-12-06 12:58:27 +0200

    fix naming comvention

--- backend/DataAccess/Interfaces/IUserRoleRepository.cs
+++ backend/DataAccess/Interfaces/IUserRoleRepository.cs
@@ -10,6 +10,6 @@ namespace Jannara_Ecommerce.DataAccess.Interfaces
         public Task<Result<bool>> UpdateAsync(int id, UserRoleDTO updatedUserRole);
         public Task<Result<bool>> DeleteAsync(int id);
         public Task<Result<UserRoleDTO>> GetByIdAsync(int id);
-        public Task<Result<IEnumerable<UserRoleDTO>>> GetAllAsync(int user_id);
+        public Task<Result<IEnumerable<UserRoleDTO>>> GetAllAsync(int userId);
     }
 }
