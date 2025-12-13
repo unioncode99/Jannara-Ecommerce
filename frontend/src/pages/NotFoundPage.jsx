76114cc47152303d6d3dc5bf1dc84ee06c9a8634
muzamil <muzamilalfatih123@gmail.com>
2025-12-13 13:23:44 +0200

    add translation to not found page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFoundPage.css";
+import { useLanguage } from "../hooks/useLanguage";
 
 const NotFoundPage = () => {
+  const { translations } = useLanguage();
   return (
     <div className="not-found-page">
       <h1>404</h1>
-      <h2>Page Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go Back Home</Link>
+      <h2>{translations.general.pages.Not_Found.title}</h2>
+      <p>{translations.general.pages.Not_Found.subtitle}</p>
+      <Link to="/">{translations.general.pages.Not_Found.home_button}</Link>
     </div>
   );
 };
