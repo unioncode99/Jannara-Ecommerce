ac0050028db0f809432b19ca6c0b2feb78a2ffe6
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-20 12:25:03 +0200

    Modify ProductList Component for resuability

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import ProductList from "../components/ProductList";
 import ProductSortingFiltersDropdown from "../components/ProductSortingFiltersDropdown";
 import Button from "../components/ui/Button";
 import Pagination from "../components/ui/Pagination";
+import { create, read, remove } from "../api/apiWrapper";
+import SpinnerLoader from "../components/ui/SpinnerLoader";
 
 const Home = () => {
   const [searchText, setSearchText] = useState("");
@@ -15,6 +17,12 @@ const Home = () => {
   const [selectProductCategoryId, setSelectProductCategoryId] = useState(-1);
   const [sortingTerm, setSortingTerm] = useState("");
   const { translations } = useLanguage();
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10; // Items per page
+  const customerId = -1;
 
   const handleSearchInputChange = (e) => {
     console.log("search -> ", e.target.value);
@@ -29,6 +37,56 @@ const Home = () => {
     return () => clearTimeout(timer);
   }, [searchText]);
 
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setIsLoading(true);
+        const queryParams = new URLSearchParams();
+        // Pagination
+        queryParams.append("pageNumber", currentPage);
+        queryParams.append("pageSize", pageSize);
+        // Optional search term
+        if (debouncedSearch && debouncedSearch.trim() !== "") {
+          queryParams.append("searchTerm", debouncedSearch.trim());
+        }
+        // Optional categoryId (assume -1 or null = no filter)
+        if (selectProductCategoryId && selectProductCategoryId !== -1) {
+          queryParams.append("categoryId", selectProductCategoryId);
+        }
+        // Optional sort
+        if (sortingTerm && sortingTerm.trim() !== "") {
+          queryParams.append("SortBy", sortingTerm.trim());
+        }
+        // Optional customerId (if needed)
+        if (customerId) {
+          queryParams.append("customerId", customerId);
+        }
+        // Final URL
+        const url = `products?${queryParams.toString()}`;
+        const data = await read(url);
+        console.log("data ->", data);
+        console.log("isSuccess ->", data.isSuccess);
+        setProducts(data?.data?.items);
+        setTotalProducts(data?.data?.total);
+        console.log("data?.items -> ", data?.data?.items);
+      } catch (error) {
+        console.error("Failed to fetch categories:", error);
+        setProducts([]);
+        setTotalProducts(0);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getProducts();
+  }, [
+    debouncedSearch,
+    selectProductCategoryId,
+    customerId,
+    sortingTerm,
+    currentPage,
+    pageSize,
+  ]);
+
   const handleProductCategoryChange = (e) => {
     console.log("category -> ", e.target.value);
     setSelectProductCategoryId(e.target.value);
@@ -37,6 +95,38 @@ const Home = () => {
   const handleSortingTermChange = (e) => {
     console.log("Sorting Term -> ", e.target.value);
     setSortingTerm(e.target.value);
+  };
+
+  // Handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleToggleFavorite = async (productId, isFavorite) => {
+    console.log("Favorite changed:", productId, isFavorite);
+    setProducts((prev) =>
+      prev.map((p) => (p.id === productId ? { ...p, isFavorite } : p))
+    );
+    try {
+      if (isFavorite) {
+        await create("customer-wish-list", {
+          customerId: 5,
+          productId: productId,
+        });
+      } else {
+        await remove("customer-wish-list", {
+          customerId: 5,
+          productId: productId,
+        });
+      }
+    } catch (error) {
+      console.error("Favorite error:", error);
+      setProducts((prev) =>
+        prev.map((p) =>
+          p.id === productId ? { ...p, isFavorite: !isFavorite } : p
+        )
+      );
+    }
   };
 
   return (
@@ -62,12 +152,39 @@ const Home = () => {
           onChange={handleSortingTermChange}
         />
       </div>
-      <div></div>
-      <ProductList
-        searchTerm={debouncedSearch}
-        categoryId={selectProductCategoryId}
-        sortingTerm={sortingTerm}
-      />
+      {isLoading ? (
+        <div className="loader-container">
+          <SpinnerLoader />
+        </div>
+      ) : (
+        <div>
+          <div>
+            <Pagination
+              currentPage={currentPage}
+              totalItems={totalProducts}
+              onPageChange={handlePageChange}
+              pageSize={pageSize}
+            />
+          </div>
+          <ProductList
+            products={products}
+            isLoading={isLoading}
+            totalProducts={totalProducts}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            onPageChange={handlePageChange}
+            onToggleFavorite={handleToggleFavorite}
+          />
+          <div>
+            <Pagination
+              currentPage={currentPage}
+              totalItems={totalProducts}
+              onPageChange={handlePageChange}
+              pageSize={pageSize}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 };
