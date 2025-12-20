d33ddb201ab4f46360507cd6ae032e2fe5ba2358
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-20 11:34:15 +0200

    Add add and remove to wish list endpoints

---
+++ backend/Business/Services/CustomerWishlistService.cs
@@ -0,0 +1,32 @@
+﻿using Jannara_Ecommerce.Business.Interfaces;
+using Jannara_Ecommerce.DataAccess.Interfaces;
+using Jannara_Ecommerce.DTOs.CustomerWishlist;
+using Jannara_Ecommerce.Utilities;
+
+namespace Jannara_Ecommerce.Business.Services
+{
+    public class CustomerWishlistService : ICustomerWishlistService
+    {
+        private readonly ICustomerWishlistRepository _customerWishlistRepository;
+
+        public CustomerWishlistService(ICustomerWishlistRepository customerWishlistRepository)
+        {
+            _customerWishlistRepository = customerWishlistRepository;
+        }
+
+        public async Task<Result<CustomerWishlistDTO>> AddNewAsync(CustomerWishlistCreateDTO newCustomerWishlist)
+        {
+            return await _customerWishlistRepository.AddNewAsync(newCustomerWishlist);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(int id)
+        {
+            return await _customerWishlistRepository.DeleteAsync(id);
+        }
+
+        public async Task<Result<bool>> DeleteAsync(CustomerWishlistCreateDTO customerWishlist)
+        {
+            return await _customerWishlistRepository.DeleteAsync(customerWishlist);
+        }
+    }
+}
