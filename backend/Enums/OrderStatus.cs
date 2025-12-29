59c54093f6ca3f88e3cf0b36f8f638ca958a11ef
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-29 14:21:08 +0200

    add OrderStatus enum

---
+++ backend/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Jannara_Ecommerce.Enums
+{
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Processing = 2,
+        Shipped = 3,
+        Delivered = 4,
+        Cancelled = 5,
+    }
+}
