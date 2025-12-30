5c43ef1c27297829ee88a4e850e65ad5a5ce0d83
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2025-12-30 13:33:46 +0200

    Modify get customer orders and get order by public id to get seller orders and it's items

---
+++ backend/Dtos/Seller/SellerDetailsDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Jannara_Ecommerce.DTOs.Seller
+{
+    public class SellerDetailsDTO
+    {
+        public long Id { get; set; }
+        public string BusinessName { get; set; }
+        public string WebsiteUrl { get; set; }
+
+        public long UserId { get; set; }
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ backend/Dtos/SellerOrderItem/SellerOrderItemDetailsDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Jannara_Ecommerce.DTOs.SellerOrderItem
+{
+    public class SellerOrderItemDetailsDTO
+    {
+        public int Id { get; set; }
+        public int SellerProductId { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
+        public string DefaultImageUrl { get; set; }
+        public string Sku { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
