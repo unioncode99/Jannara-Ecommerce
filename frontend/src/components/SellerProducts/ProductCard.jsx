7f27f607710878d714e0cbfa23731840b2252294
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-01-17 15:40:49 +0200

    Add table view to seller product page

--- frontend/src/components/SellerProducts/ProductCard.jsx
+++ frontend/src/components/SellerProducts/ProductCard.jsx
@@ -1,8 +1,18 @@
 import { Pencil, Trash2 } from "lucide-react";
 import { useLanguage } from "../../hooks/useLanguage";
+import { formatMoney } from "../../utils/utils";
 
 const ProductCard = ({ product, handleEditProduct, handleDeleteProduct }) => {
-  const { language } = useLanguage();
+  const { language, translations } = useLanguage();
+
+  const {
+    name_label,
+    category_label,
+    brand_label,
+    sku_label,
+    price_label,
+    stock_label,
+  } = translations.general.pages.seller_products;
 
   return (
     <div className="product-card">
@@ -11,15 +21,26 @@ const ProductCard = ({ product, handleEditProduct, handleDeleteProduct }) => {
       </div>
       <div className="product-info">
         <h3 className="product-name">
+          {name_label}:{" "}
           {language == "en" ? product.productNameEn : product.productNameAr}
         </h3>
         <p className="product-category">
+          {category_label}:{" "}
           {language == "en" ? product.categoryNameEn : product.categoryNameAr}
         </p>
         <p className="product-brand">
+          {brand_label}:{" "}
           {language == "en" ? product.brandNameEn : product.brandNameAr}
         </p>
-        <p className="product-sku">{product.sku}</p>
+        <p className="product-sku">
+          {sku_label}: {product.sku}
+        </p>
+        <p className="seller-product-price">
+          {price_label}: {formatMoney(product.price)}
+        </p>
+        <p className="product-stock">
+          {stock_label}: {product.stockQuantity}
+        </p>
       </div>
       <div className="product-actions">
         <button
--- frontend/src/components/SellerProducts/TableView.jsx
+++ frontend/src/components/SellerProducts/TableView.jsx
@@ -1,4 +1,82 @@
+import { Pencil, Trash2 } from "lucide-react";
+import { useLanguage } from "../../hooks/useLanguage";
+import Table from "../ui/Table";
+import "./TableView.css";
+import { formatMoney } from "../../utils/utils";
+
 const TableView = ({ products, handleEditProduct, handleDeleteProduct }) => {
-  return <div>TableView</div>;
+  const { translations, language } = useLanguage();
+  const { product_name, category, brand, actions } =
+    translations.general.pages.products;
+
+  const {
+    name_label,
+    category_label,
+    brand_label,
+    sku_label,
+    price_label,
+    stock_label,
+  } = translations.general.pages.seller_products;
+
+  return (
+    <div className="table-view">
+      <Table
+        headers={[
+          product_name,
+          category,
+          brand,
+          "sku",
+          price_label,
+          stock_label,
+          actions,
+        ]}
+        data={products.map((product) => ({
+          product_name: (
+            <div className="product-name-container">
+              <span className="product-image-container">
+                <img src={product.productImage} alt="Profile Image" />
+              </span>
+              <div>
+                {language == "en"
+                  ? product.productNameEn
+                  : product.productNameAr}
+              </div>
+            </div>
+          ),
+          category: (
+            <small>
+              {language == "en"
+                ? product.categoryNameEn
+                : product.categoryNameAr}
+            </small>
+          ),
+          brand: (
+            <small>
+              {language == "en" ? product.brandNameEn : product.brandNameAr}
+            </small>
+          ),
+          sku: <small>{product.sku}</small>,
+          price_label: <small>{formatMoney(product.price)}</small>,
+          stock_label: <small>{product.stockQuantity}</small>,
+          actions: (
+            <div className="product-actions">
+              <button
+                onClick={() => handleEditProduct(product)}
+                className="edit-product-btn"
+              >
+                <Pencil />
+              </button>
+              <button
+                onClick={() => handleDeleteProduct(product)}
+                className="delete-product-btn"
+              >
+                <Trash2 />
+              </button>
+            </div>
+          ),
+        }))}
+      />
+    </div>
+  );
 };
 export default TableView;
